6e675762b5e77305c801ea63e57941845b982de1
Gang_Lu <gang_lu@asdc.com.cn>
2018-08-31 18:06:07 +0800

    test log4cxx.

---
+++ test_log4cxx/bar.cpp
@@ -0,0 +1,12 @@
+// file bar.cpp
+#include "bar.h"
+
+using namespace com::foo;
+using namespace log4cxx;
+
+LoggerPtr Bar::logger(Logger::getLogger("com.foo.bar"));
+
+
+void Bar::doIt() {
+	LOG4CXX_DEBUG(logger, "Did it again!");
+}
